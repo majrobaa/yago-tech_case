93fd01b4f7be08f55e42f74badebbeaa29d539b7
Robi <r.majnik@vtx.hu>
2022-10-14 12:15:04 +0200

    Documentation needed

--- includes/preload.php
+++ includes/preload.php
@@ -3,7 +3,6 @@ include_once "config.php";
 include_once "class/navigate.php";
 session_start();
 
-
 spl_autoload_register(function ($cname) {
     if (is_file(CLASSFOLDER . $cname . ".php")) {
         require_once CLASSFOLDER . $cname . ".php";
